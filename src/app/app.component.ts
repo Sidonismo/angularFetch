db0fc1c069758200a6a7eb2cd240eb6e156308d3
Eliáš Sidon <sidonismo@gmail.com>
2021-01-29 00:07:04 +0100

    zkoušení regex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,11 @@ import { NameService } from './name.service';
 })
 export class AppComponent {
   title = 'restApi';
-  response = '';
+  zprava = '';
+  jeden_znak = '';
+  alfabeta = '';
+  zacina_cislem = '';
+  nealfanumericke = '';
   constructor(private nameService:NameService){
 
   }
@@ -17,11 +21,37 @@ export class AppComponent {
     formData.append('name', name)
     this.nameService.onSendService(formData).subscribe
     (res=>{
+      let zprava;
+      let jeden_znak;
+      let alfabeta;
+      let zacina_cislem;
+      let nealfanumericke;
       console.log(res);
-      this.response = res
+      if (res.povedene === true){
+        
+        zprava = res.zprava;
+        jeden_znak = res.jeden_znak;
+        alfabeta = res.alfabeta;
+        zacina_cislem = res.zacina_cislem;
+        nealfanumericke = res.nealfanumericke;
+        this.zprava = zprava;
+        this.jeden_znak = jeden_znak;
+        this.alfabeta = alfabeta;
+        this.zacina_cislem = zacina_cislem;
+        this.nealfanumericke = nealfanumericke;
+      }
+      else if (res.povedene === false){
+        this.zprava = 'Musíte zadat minimálně jeden znak!';
+        this.jeden_znak = '';
+        this.alfabeta = '';
+        this.zacina_cislem = '';
+        this.nealfanumericke = '';
+        return;
+      }
     },
     err=>{
-      console.log(err);
+      console.log(err.message);
+      this.zprava = 'Nastala chyba!';
     })
   }
 }
