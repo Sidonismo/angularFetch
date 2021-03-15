a114ddb88dc494bdd2bee2a2288a78767ade0ef1
Eliáš Sidon <sidonismo@gmail.com>
2021-03-15 16:29:38 +0100

    uprava url api

--- src/app/name.service.ts
+++ src/app/name.service.ts
@@ -11,7 +11,7 @@ export class NameService {
 
   onSendService(fromData: FormData): Observable<any> {
     if (/github/.test(location.hostname)) {
-      return this.http.post<any>('https://pwapps.net/regex.php', fromData);
+      return this.http.post<any>('https://pwapps.net/api/regex.php', fromData);
     }
     else {
       return this.http.post<any>('http://localhost:3006/angular.php', fromData);
